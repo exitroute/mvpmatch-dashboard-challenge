7c8ba3c13b4d1b5d9b8a1ba49b51937fbeba0851
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-09 21:24:16 +0100

    add swr config to make fetcher global

--- pages/reports.tsx
+++ pages/reports.tsx
@@ -2,13 +2,22 @@ import type { NextPage } from "next";
 import Layout from "../components/Layout";
 import ReportsMenu from "../components/ReportsMenu";
 import ReportsDisplay from "../components/ReportsDisplay";
+import useSWR, { SWRConfig } from "swr";
 
 const Reports: NextPage = () => {
   return (
-    <Layout>
-      <ReportsMenu />
-      <ReportsDisplay />
-    </Layout>
+    <SWRConfig
+      value={{
+        refreshInterval: 3000,
+        fetcher: (arg: any, ...args: any) =>
+          fetch(arg, ...args).then((res) => res.json()),
+      }}
+    >
+      <Layout>
+        <ReportsMenu />
+        <ReportsDisplay />
+      </Layout>
+    </SWRConfig>
   );
 };
 
