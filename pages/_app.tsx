907536cd285875bb61213fb0f0d15ebebf1be3cf
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-09 12:46:35 +0100

    add context to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppWrapper } from "../context/state";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <AppWrapper>
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
     </AppWrapper>
+  );
 }
 
-export default MyApp
+export default MyApp;
