72b98da21e88c1dd93a999f617a6b4738f9f3b3e
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-09 23:57:38 +0100

    make context only for reports

--- pages/reports.tsx
+++ pages/reports.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import { ReportsProvider } from "../context/ReportsContext";
 import Layout from "../components/Layout";
 import ReportsMenu from "../components/ReportsMenu";
 import ReportsDisplay from "../components/ReportsDisplay";
@@ -6,18 +7,20 @@ import useSWR, { SWRConfig } from "swr";
 
 const Reports: NextPage = () => {
   return (
-    <SWRConfig
-      value={{
-        refreshInterval: 3000,
-        fetcher: (arg: any, ...args: any) =>
-          fetch(arg, ...args).then((res) => res.json()),
-      }}
-    >
-      <Layout>
-        <ReportsMenu />
-        <ReportsDisplay />
-      </Layout>
-    </SWRConfig>
+    <ReportsProvider>
+      <SWRConfig
+        value={{
+          refreshInterval: 3000,
+          fetcher: (arg: any, ...args: any) =>
+            fetch(arg, ...args).then((res) => res.json()),
+        }}
+      >
+        <Layout>
+          <ReportsMenu />
+          <ReportsDisplay />
+        </Layout>
+      </SWRConfig>
+    </ReportsProvider>
   );
 };
 
