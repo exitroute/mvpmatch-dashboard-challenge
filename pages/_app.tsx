b8f5b6ed6790a934088dbf0c03946813204bb42c
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-09 12:45:58 +0100

    add chakra to app component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppWrapper } from "../context/state";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AppWrapper>
 }
 
 export default MyApp
