f71198a55b2233eca5d3a0e45dde2dafac02c215
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-09 23:58:47 +0100

    remove import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { AppWrapper } from "../context/ReportsContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
