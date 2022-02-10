21f13ddeaa657ee01359e4f82b73c3d5dfdd7f30
Ryan O'Shea <ryan.oshea@exitroute.org>
2022-02-10 18:22:59 +0100

    add all swr call to report pages so data can be used at app level

--- pages/reports.tsx
+++ pages/reports.tsx
@@ -1,25 +1,38 @@
 import type { NextPage } from "next";
 import { ReportsProvider } from "../context/ReportsContext";
+import { useAppContext } from "../context/AppContext";
 import Layout from "../components/Layout";
 import ReportsMenu from "../components/ReportsMenu";
 import ReportsDisplay from "../components/ReportsDisplay";
-import useSWR, { SWRConfig } from "swr";
+import useSWR from "swr";
 
 const Reports: NextPage = () => {
+  const { getUserData, getProjectData, getGatewayData } = useAppContext();
+
+  const url = `http://178.63.13.157:8090/mock-api/api`;
+
+  const { data: users, error: fetchUserDataError } = useSWR(`${url}/users`);
+  if (fetchUserDataError) console.error(fetchUserDataError);
+  getUserData(users.data);
+
+  const { data: projects, error: fetchProjectDataError } = useSWR(
+    `${url}/projects`
+  );
+  if (fetchProjectDataError) console.error(fetchProjectDataError);
+  getProjectData(projects.data);
+
+  const { data: gateways, error: fetchGatewayDataError } = useSWR(
+    `${url}/gateways`
+  );
+  if (fetchGatewayDataError) console.error(fetchGatewayDataError);
+  getGatewayData(gateways.data);
+
   return (
     <ReportsProvider>
-      <SWRConfig
-        value={{
-          refreshInterval: 3000,
-          fetcher: (arg: any, ...args: any) =>
-            fetch(arg, ...args).then((res) => res.json()),
-        }}
-      >
-        <Layout>
-          <ReportsMenu />
-          <ReportsDisplay />
-        </Layout>
-      </SWRConfig>
+      <Layout>
+        <ReportsMenu />
+        <ReportsDisplay />
+      </Layout>
     </ReportsProvider>
   );
 };
