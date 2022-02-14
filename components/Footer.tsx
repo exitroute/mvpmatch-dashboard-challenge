d1dd772aa37ebc580fd4c89199526dd69d4af9ec
Ryan O'Shea <ryan@ryanoshea.dev>
2022-02-14 21:40:06 +0100

    typo fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@chakra-ui/layout";
 
 export const Footer = () => {
-  return <Box p="2rem"> "Terms & Conditions | Privacy Policy</Box>;
+  return <Box p="2rem">Terms & Conditions | Privacy Policy</Box>;
 };
